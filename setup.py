fc18717fbbae035d501e133815996007474bbfec
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-05-05 17:29:15 +0200

    add setup and __init__ for pip installation

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+requirements = ["numpy"]
+
+setuptools.setup(
+    name="lii",
+    version="1.0.0",
+    author="Cyril Meyer",
+    author_email="contact@cyrilmeyer.eu",
+    description="Large Image Inference",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cyril-Meyer/lii",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+    ],
+)
